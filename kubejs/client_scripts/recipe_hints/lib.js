906fc35a701faa9b253203d16278fa1534669c04
ZZZank <3410764033@qq.com>
2025-01-26 00:05:25 +0800

    forgot to commit util functions

--- kubejs/client_scripts/recipe_hints/lib.js
+++ kubejs/client_scripts/recipe_hints/lib.js
@@ -1,6 +1,21 @@
 // priority: 1000
 
 /**
+ * @param {any[][]} arrays
+ * @param {number} minSize
+ * @param {any} toFill
+ * @return {any[][]} the `arrays` itself
+ */
+function ensureSubArraySize(arrays, minSize, toFill) {
+    for (let arr of arrays) {
+        while (arr.length < minSize) {
+            arr.push(toFill)
+        }
+    }
+    return arrays
+}
+
+/**
  * arrow: width=24, height=17
  * @param {number} start 
  * @param {number} width 
@@ -11,39 +26,69 @@ function computeArrowPos(start, width) {
 }
 
 /**
- * @param {string} id
+ * split an array into many `pages`
+ * 
+ * example: the result of `splitArray([1,2,3,4,5,6,7], 3)` is `[[1,2,3],[4,5,6],[7]]`
+ * @param {any[]} arr 
+ * @param {number} size 
+ * @return {any[]}
  */
-function KJEICategory(id) {
-    this.id = new ResourceLocation(id)
-    this.catalysts = []
-}
-
-KJEICategory.prototype.addCatalyst = function (catalyst) {
-    this.catalysts.push(Item.of(catalyst))
+function splitArray(arr, size) {
+    const pages = []
+    let page = []
+    const arrLen = arr.length
+    let counter = 0
+    for (let i = 0; i < arr.length; i++) {
+        page.push(arr[i])
+        counter++
+        if (counter === size) {
+            counter = 0
+            pages.push(page)
+            page = []
+        }
+    }
+    if (page.length != 0) {
+        pages.push(page)
+    }
+    return pages
 }
 
 /**
- * @param {(event: $RegisterCategoriesEventJS_) => $IDrawable_} icon 
+ * @param {any[]} arr
+ * @param {number} size
+ * @param {any} fillValue
  */
-KJEICategory.prototype.setIcon = function (icon) {
-    this.iconMapper = icon
+function splitArraySizeEnsured(arr, size, fillValue) {
+    const split = splitArray(arr, size)
+    if (split.length == 0) {
+        return []
+    }
+    const toFill = split[split.length - 1]
+    while (toFill.length < size) {
+        toFill.push(fillValue)
+    }
+    return split
 }
 
 /**
- * @param {$Text_} title 
+ * spread an array into many `pages`, where the `i`-th element will be in `i % spreadLength`-th page
+ * 
+ * example: the result of `splitArray([1,2,3,4,5,6,7], 3)` is `[[1,4,7],[2,5],[3,6]]`
+ * @param {any[]} arr
+ * @param {any} spreadLength
  */
-KJEICategory.prototype.setTitle = function (title) {
-    this.title = title
+function spreadArray(arr, spreadLength) {
+    const toSpread = Array(spreadLength).map(() => [])
+    arr.forEach((value, i) => toSpread[i % spreadLength] = value)
+    return toSpread
 }
 
-KJEICategory.prototype.register = function () {
-    onEvent('kube_jei.register_recipe_catalysts', event => {
-        event.addItemCatalyst(this.catalysts, this.id)
-    })
-    onEvent('kube_jei.register_categories', event => {
-        event.custom(this.id)
-            .setIcon(this.iconMapper(event))
-            .setTitle(this.title)
-            .setBackground
-    })
+/**
+ * @param {any[]} arr
+ * @param {number} size
+ * @param {any} fillValue
+ */
+function spreadArraySizeEnsured(arr, size, fillValue) {
+    const split = spreadArray(arr, size)
+    return ensureSubArraySize(split, size, fillValue)
 }
