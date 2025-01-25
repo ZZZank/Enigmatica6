f25cbd12c6aecd96e941276bffb41ea7e74c9cea
ZZZank <3410764033@qq.com>
2025-01-26 00:20:34 +0800

    remove custom machinery

--- kubejs/server_scripts/helper/custommachinery_recipe.js
+++ kubejs/server_scripts/helper/custommachinery_recipe.js
@@ -3,18 +3,3 @@
 function addGeneralRecipeHint(recipe, event, additional) {
 
 }
-
-/**
- * Gets a ItemStackJS with its `chance` property displayed in its name, or itself if
- * its chance is not specified.
- *
- * `chance` will be displayed in `{name} {chance}%` format
- * @param {Internal.ItemStackJS} item
- */
-const withChanceInName = (item) => {
-    const chance = item.getChance();
-    if (!chance) {
-        return item;
-    }
-    return item.withName(item.name.append(text.of(' ' + (chance * 100).toPrecision(3) + '%').gray()));
-};
