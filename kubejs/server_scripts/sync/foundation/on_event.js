b0f51c625951c07a10d98b0aea09e90507ee44a8
ZZZank <3410764033@qq.com>
2025-02-21 15:08:29 +0800

    try to prevent registering unused event handler early

---
+++ kubejs/server_scripts/sync/foundation/on_event.js
@@ -0,0 +1,19 @@
+// priority: 1009
+
+/**
+ * @type {typeof onEvent}
+ */
+const onEventExpert = (id, handler) => {
+    if (isExpertMode) {
+        onEvent(id, handler)
+    }
+}
+
+/**
+ * @type {typeof onEvent}
+ */
+const onEventNormal = (id, handler) => {
+    if (isNormalMode) {
+        onEvent(id, handler)
+    }
+}
