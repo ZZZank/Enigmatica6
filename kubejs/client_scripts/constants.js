0f87df47d95e688f2643eef4198de62a1f36ebef
ZZZank <3410764033@qq.com>
2025-03-02 00:40:27 +0800

    fixing

--- kubejs/client_scripts/constants.js
+++ kubejs/client_scripts/constants.js
@@ -1,36 +1,5 @@
 // priority: 1000
 
-const air = 'minecraft:air';
-
-// priority of which mod output should come from, if applicable
-const modPriorities = [
-    'emendatusenigmatica',
-    'minecraft',
-    'immersiveengineering',
-    'thermal',
-    'mekanism',
-    'kubejs',
-    'pneumaticcraft',
-    'create',
-    'occultism',
-    'industrialforegoing',
-    'botania',
-    'quark',
-    'pedestals',
-    'appliedenergistics2',
-    'mapperbase',
-    'bloodmagic',
-    'morevanillalib',
-    'titanium',
-    'mythicbotany',
-    'undergarden',
-    'byg',
-    'atum',
-    'betterendforge',
-    'miniutilities',
-    'chipped',
-    'tconstruct'
-];
 const refinedStorageItems = [
     'grid',
     'crafting_grid',
